0506155ec9507bb5d8b3a4f45396d210d851023f
Timilehin Arigbede <harriemannie@gmail.com>
2024-06-01 11:33:14 +0100

    Creating Consistent error structure and converting Errors to Responses

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorhandler = (
   err: Error,
@@ -6,8 +8,22 @@ export const errorhandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong: ", err);
+  // Handling request validation errors
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.type };
+    });
+
+    return res.status(400).send({ errors: formattedErrors });
+  }
+
+  // Handling database errors
+
+  if (err instanceof DatabaseConnectionError) {
+    res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: "Something went wrong" }],
   });
 };
