4439bf47929296a3d8b7630f3239d6f527a49807
Timilehin Arigbede <harriemannie@gmail.com>
2024-06-03 14:02:09 +0100

    Creating CurrentUser Middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,24 +1,15 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    // If the user is not logged in, return null
-    return res.send({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  // If the user is logged in, verify the JWT token
-  try {
-    // If the token is invalid, an error will be thrown
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    return res.send({ currentUser: null });
-  }
-
-  res.send("Hi there!");
-});
+);
 
 export { router as currentUserRouter };
