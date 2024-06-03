2a3a62d8b195244ab7deaa464fee5e49b5c3b97e
Timilehin Arigbede <harriemannie@gmail.com>
2024-06-03 13:06:29 +0100

    Handling the CurrentUser Route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    // If the user is not logged in, return null
+    return res.send({ currentUser: null });
+  }
+
+  // If the user is logged in, verify the JWT token
+  try {
+    // If the token is invalid, an error will be thrown
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    return res.send({ currentUser: null });
+  }
+
   res.send("Hi there!");
 });
 
