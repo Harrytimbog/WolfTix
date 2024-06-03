a8640a1d1aa1e70ed0db8b998361d23dc7062bdb
Timilehin Arigbede <harriemannie@gmail.com>
2024-06-03 13:11:35 +0100

    Signout Logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
+  // Destroy the session to log the user out
+  req.session = null;
+  // Send an empty object to indicate that the user has been logged out
+  res.send({});
 });
 
 export { router as signoutRouter };
