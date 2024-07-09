af072ff16cfc44321d105cc0d09abb09fc7b8932
Timilehin Arigbede <harriemannie@gmail.com>
2024-07-10 00:15:33 +0100

    Specifying the Host

--- client/app/page.js
+++ client/app/page.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { headers } from "next/headers";
 import React from "react";
 
 export default async function Home() {
   // Get User
   const user = await getUser();
-
+  console.log(user);
   return (
     <main className="container mt-5">
       <h1 className="text-center">
@@ -15,11 +16,24 @@ export default async function Home() {
 }
 
 export async function getUser() {
- // Reach our ingress-nginx-controller service from inside the client pod
-  const serviceName = http://ingress-nginx-controller.ingress-nginx.svc.cluster.local;
+  if (typeof window === "undefined") {
+    // Reach our ingress-nginx-controller service from inside the client pod
+    const serviceName =
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local";
 
+    const { data } = await axios.get(`${serviceName}/api/users/currentUser`, {
+      headers: {
+        Host: "wolfticketing.dev",
+      },
+    });
 
-  const response = await axios.get(`${serviceName}/api/users/currentUser`);
+    return data;
+  } else {
+    // We are on the browser
 
-  return response.data;
+    // request can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentUser");
+    return data;
+  }
+  return {};
 }
