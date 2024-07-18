e275e0a3b7cce9ba2ff7990ce6ad3b42d0814dbc
Timilehin Arigbede <harriemannie@gmail.com>
2024-07-18 20:06:44 +0100

    Complete Index Route Implementation

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,7 @@ import cookieSession from "cookie-session";
 
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 // Middlewares
 import {
@@ -33,6 +34,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
