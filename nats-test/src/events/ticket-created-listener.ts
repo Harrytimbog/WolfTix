058465dd8260cba075ccd07a51539603d35aea39
Timilehin Arigbede <harriemannie@gmail.com>
2024-07-25 18:31:10 +0100

    Building a NATS Test Project | Quick refactor

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,12 @@
+import { JsMsg } from "nats";
+import { Listener } from "./base-listener";
+
+export class TicketCreatedListener extends Listener {
+  subject = "ticket.created";
+  queueGroupName = "orders-service-queue-group";
+
+  onMessage(data: any, msg: JsMsg) {
+    console.log(`Received event with data: ${JSON.stringify(data)}`);
+    msg.ack();
+  }
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -7,63 +7,12 @@ import {
   JsMsg,
 } from "nats";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: JsMsg): void;
-  private client: NatsConnection;
-  private subscription?: JetStreamSubscription;
-
-  constructor(client: NatsConnection) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    const opts = consumerOpts();
-    opts.ackExplicit();
-    opts.deliverNew(); // Deliver only new messages
-    opts.manualAck(); // Ensure manual acknowledgment is set
-    opts.durable(this.queueGroupName); // Set the durable name
-    opts.queue(this.queueGroupName); // Set the queue group name
-    opts.deliverTo(this.queueGroupName + "_subject"); // Set the deliver subject
-    return opts;
-  }
-
-  async listen() {
-    const js = this.client.jetstream();
-    this.subscription = await js.subscribe(
-      this.subject,
-      this.subscriptionOptions()
-    );
-
-    const sc = StringCodec();
-
-    for await (const msg of this.subscription) {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    }
-  }
-
-  parseMessage(msg: JsMsg) {
-    const data = msg.data;
-    return JSON.parse(new TextDecoder().decode(data));
-  }
-}
 
 // Concrete Listener Implementation
-class TicketCreatedListener extends Listener {
-  subject = "ticket.created";
-  queueGroupName = "orders-service-queue-group";
-
-  onMessage(data: any, msg: JsMsg) {
-    console.log(`Received event with data: ${JSON.stringify(data)}`);
-    msg.ack();
-  }
-}
 
 const startListener = async () => {
   const clientId = randomBytes(12).toString("hex");
