a960e810e8389cacc3ab4a493c52070aad9fdb08
Timilehin Arigbede <harriemannie@gmail.com>
2024-08-28 21:17:15 +0100

    Skaffolding the Orders Service

---
+++ orders/src/app.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+// Routes
+
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+// Middlewares
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@clonedwolftickets/common";
+
+const app = express();
+// Trust the ingress-nginx proxy
+app.set("trust proxy", true);
+app.use(json());
+
+// Disable the secure flag for the cookie session because this services might be needed
+// to be accessed from other services that are written in different languages
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+// Route handlers
+
+// set the current user property on the request object
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+// export the app
+
+export { app };
