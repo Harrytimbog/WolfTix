313341fde263e94c8c5359dc42f6109f85eb3cf6
Timilehin Arigbede <harriemannie@gmail.com>
2024-08-28 20:21:28 +0100

    Ensuring Mock Invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -82,4 +83,16 @@ it("creates a ticket with valid inputs", async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
   expect(tickets[0].title).toEqual(title);
+});
+
+it("publishes an event", async () => {
+  const title = "alaskaaa";
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title, price: 20 })
+    .expect(201);
+
+  // Check that publish was called on the jsClient
+  expect(natsWrapper.jsClient.publish).toHaveBeenCalled();
 });
