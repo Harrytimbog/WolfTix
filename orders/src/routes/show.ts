46edf32d81e31eb5941901a8221ffe6436269c83
Timilehin Arigbede <harriemannie@gmail.com>
2024-08-29 18:37:43 +0100

    Cancelling an Order

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -19,7 +19,7 @@ router.get(
       throw new NotFoundError();
     }
 
-    if (order.userId != req.currentUser!.id) {
+    if (order.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
 
