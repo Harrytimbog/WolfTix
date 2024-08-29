fe2f026d35ef145149f7f69c853378b78f277c9a
Timilehin Arigbede <harriemannie@gmail.com>
2024-08-29 20:15:22 +0100

    Publishing the Order Creation and Order Cancellation

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,6 +5,8 @@ import {
   requireAuth,
 } from "@clonedwolftickets/common";
 import { Order, OrderStatus } from "../models/order";
+import { OrderCalledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -13,7 +15,7 @@ router.delete(
   requireAuth,
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) throw new NotFoundError();
 
@@ -25,6 +27,12 @@ router.delete(
     await order.save();
 
     // Publish an event saying this was cancelled!
+    await new OrderCalledPublisher(natsWrapper.jsClient).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
     res.status(204).send(order);
   }
 );
