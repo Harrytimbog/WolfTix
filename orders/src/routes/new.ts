ee054ff21ed2354d7b92790039c5d8946814886f
Timilehin Arigbede <harriemannie@gmail.com>
2024-08-29 10:25:02 +0100

    Order Model Setup

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@clonedwolftickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send("Orders route");
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // Not so ideal because Ticket Service might change the format of the ticketId (Not all services will use mongoDB)
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send("Orders route");
+  }
+);
 
 export { router as newOrderRouter };
