10b5e136e1272af3883c0fa823e1419c519e2095
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-03 14:41:38 +0100

    Testing OOC (Optimisitic Concurrency Control) with Mongoose | Versioning...OD

--- tickets/src/models/ticket.test.ts
+++ tickets/src/models/ticket.test.ts
@@ -1,9 +1,30 @@
 import { Ticket } from "./ticket";
 
-// try {
-//   await secondInstance!.save();
-// } catch (err) {
-//   return;
-// }
+it("implements optimistic concurrency control", async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 5,
+    userId: "123",
+  });
+  // save the ticket to the database
+  await ticket.save();
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+  // make two separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+  // save the first fetched ticket
+  await firstInstance!.save();
 
-it("implements optimistic concurrency control", async () => {});
+  // save the second fetched ticket and expect an error
+
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error("Should not reach this point");
+});
