d27582a017bcf7d88f479cfcbb05dfa6931d74c6
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-03 22:23:39 +0100

    Abstracted Query Method

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -13,7 +13,8 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 
   async onMessage(data: TicketUpdatedEvent["data"], msg: JsMsg) {
     const { id, title, price } = data;
-    const ticket = await Ticket.findOne({ _id: id, version: data.version - 1 });
+
+    const ticket = await Ticket.findbyEvent({ id, version: data.version });
 
     if (!ticket) {
       throw new Error("Ticket not found");
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -18,6 +18,11 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  // A function to find the ticket by event id and version
+  findbyEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -44,6 +49,14 @@ ticketSchema.set("versionKey", "version");
 
 // Add the updateIfCurrentPlugin to the schema
 ticketSchema.plugin(updateIfCurrentPlugin);
+
+// A function to find the ticket by event id and version
+ticketSchema.statics.findbyEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
