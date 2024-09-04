272f8cf59b398a719647da44e827fa2d6f48c151
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-04 09:13:16 +0100

    Checking Order and Ticket Listener for any fix

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { TicketDoc } from "./ticket";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { OrderStatus } from "@clonedwolftickets/common";
 
 // An interface that describes the properties that are required to create a new Order
@@ -52,6 +53,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
