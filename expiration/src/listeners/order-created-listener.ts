8c2c13250effd05f2ac84e5dae06fea49d0a0666
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-04 22:20:24 +0100

    Defining the Expiration Complete Event

--- expiration/src/listeners/order-created-listener.ts
+++ expiration/src/listeners/order-created-listener.ts
@@ -12,10 +12,17 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: JsMsg) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting this many milliseconds to process the job:", delay);
     // Add expiration logic here
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
     msg.ack();
   }
 }
