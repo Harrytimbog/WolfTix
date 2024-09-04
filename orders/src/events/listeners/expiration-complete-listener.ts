cfd9d1d384984448675516a1ba026949a90bea40
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-04 23:31:51 +0100

    Emitting the Order Cancelled Event

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,42 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+} from "@clonedwolftickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { JsMsg } from "nats";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: JsMsg) {
+    const order = await Order.findById(data.orderId).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      // not setting ticket to null so the reference to the ticket is still there
+    });
+
+    await order.save();
+
+    // publish an event saying this order was cancelled
+    // so other services can update their data
+    await new OrderCancelledPublisher(this.jsClient).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
