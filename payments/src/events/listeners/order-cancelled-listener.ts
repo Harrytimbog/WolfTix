c054607198f7a14a6fbdf3979bdea6a106391113
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-05 21:28:36 +0100

    Marking an Order as Cancelled

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,30 @@
+import {
+  OrderCancelledEvent,
+  Subjects,
+  Listener,
+  OrderStatus,
+} from "@clonedwolftickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { JsMsg } from "nats";
+import { Order } from "../../models/order";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: JsMsg) {
+    // find the order that was cancelled and update its status
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    // update the order status to cancelled
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+  }
+}
