85613df4d273e9d40aa02c60dd4d71d651bb0216
Timilehin Arigbede <harriemannie@gmail.com>
2024-09-18 13:24:36 +0100

    Make change to trigger github workflow for testing

--- client/app/page.js
+++ client/app/page.js
@@ -42,7 +42,7 @@ export default async function Home() {
 
   return (
     <main className="container mt-5">
-      <h2 className="text-center">Tickets</h2>
+      <h1 className="text-center">Tickets</h1>
       <table className="table">
         <thead>
           <tr>
